078f72625e02a6112e1a61505a0e224bb42d1660
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-05-02 02:33:05 -0400

    let's see

--- server.js
+++ server.js
@@ -1,39 +1,5 @@
-function coinFlip() {
-    return Math.random() > 0.5 ? 'heads' : 'tails'
-}
-
-function coinFlips(flips) {
-    const result = []
-    for(let i = 0; i < flips; i++){
-      result[i] = coinFlip()
-    }
-    return result
-}
-
-function countFlips(array) {
-    let head = 0
-    let tail = 0
-    array.forEach(element => {
-      if(element == "heads") head++
-      else tail++
-    });
-    if (head == 0) {
-      return {'tails': tail}
-    } else if (tail == 0) {
-      return {'heads': head}
-    } else {
-      return {'heads': head, 'tails': tail}
-    }
-}
-
-
-function flipACoin(call) {
-    let flip = coinFlip()
-    const result = ""
-    if(call == flip) return {call: call, flip: flip, result: 'win'}
-    else return {call: call, flip: flip, result: 'lose'}
-}
-
+//require express.js
+const express = require('express')
 const app = express()
 const args = minimist(process.argv.slice(2));
 const port = args.port || process.env.PORT || 5000
@@ -77,8 +43,51 @@ app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
 
-
-
 const server = app.listen(port, () => {
         console.log('App listening on port %PORT%'.replace('%PORT%',port))
 });
+//functions
+function coinFlip() {
+    if(Math.random() > .5){
+      return "tails"
+    } else {
+      return "heads"
+    }
+  }
+
+  function coinFlips(flips) {
+    let returnable = [];
+    for(let i = 0; i < flips; i++){
+      returnable.push(coinFlip());
+    } 
+    return returnable;
+  }
+
+  function countFlips(array) {
+    let heads = 0;
+    let tails = 0;
+    for(let i = 0; i < array.length; i++){
+      if (array[i] == "heads"){
+        heads += 1;
+      } else {
+        tails += 1
+      }
+    }
+    return "{ heads: " + heads + ", tails: " + tails + " }"
+  }
+
+  function flipACoin(call) {
+    let er_mes = "Error: no input." + "\n"
+    let fix_mes = "Usage: node guess-flip --call=[heads|tails]"
+    if(call != "heads" && call != "tails"){
+      return er_mes + fix_mes
+    }
+    let theFlip = coinFlip();
+    let resultW = "win"
+    let resultsL = "lose"
+    if(theFlip == call){
+      return "{ call: " + call + ", flip: " + theFlip + ", result: " + resultW + " }"
+    } else {
+      return "{ call: " + call + ", flip: " + theFlip + ", result: " + resultsL + " }"
+    }
+  }
