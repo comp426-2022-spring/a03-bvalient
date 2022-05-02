4d94dee8b166a3df891e3710aef1a88ac9eec352
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-05-02 02:05:30 -0400

    port?

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const {get} = require('http')
 
 const args = require('minimist')(process.argv.slice(2))
 args["port"]
-const port = args.port || process.env.port || 5000
+const HTTP_PORT = args.port || process.env.port || 5000
 
 //start an app server
 const server = app.listen(HTTP_PORT, () =>{
