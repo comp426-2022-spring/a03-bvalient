7231be1c96e4aae73370bfc39b912c8daffad1b3
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-05-02 01:54:05 -0400

    app.get stuff

--- server.js
+++ server.js
@@ -2,6 +2,12 @@
 const express = require('express')
 const app = express()
 
+const {get} = require('http')
+
+const args = require('minimist')(process.argv.slice(2))
+args["port"]
+const port = args.port || process.env.port || 5000
+
 //start an app server
 const server = app.listen(HTTP_PORT, () =>{
     console.log('App Listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
@@ -10,4 +16,15 @@ const server = app.listen(HTTP_PORT, () =>{
 //default response for any other request
 app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
+});
+
+
+
+app.get('/app/', (req, res) => {
+    //status 200
+    res.statusCode = 200;
+    // status message "OK"
+    res.statusMessage = 'OK';
+    res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain'});
+    res.end( res.statusCode + ' ' + res.statusMessage)
 });
