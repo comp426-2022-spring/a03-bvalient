a02560312ae90714e4ac603714e5f7f280e7c923
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-05-02 02:35:20 -0400

    mary poppins

--- server.js
+++ server.js
@@ -1,93 +1,81 @@
-//require express.js
 const express = require('express')
+const { get } = require('http')
 const app = express()
-const args = minimist(process.argv.slice(2));
-const port = args.port || process.env.PORT || 5000
 
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
+const args = require('minimist')(process.argv.slice(2))
+args["port"]
+
+const port = args.port || process.env.port || 5000
+
+const server = app.listen(port, () =>{
+    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+});
+
+
+function coinFlip() {
+    return Math.random() > 0.5 ? 'heads' : 'tails'
+}
+
+function coinFlips(flips) {
+    const result = []
+    for(let i = 0; i < flips; i++){
+      result[i] = coinFlip()
+    }
+    return result
+}
+
+function countFlips(array) {
+    let head = 0
+    let tail = 0
+    array.forEach(element => {
+      if(element == "heads") head++
+      else tail++
     });
+    if (head == 0) {
+      return {'tails': tail}
+    } else if (tail == 0) {
+      return {'heads': head}
+    } else {
+      return {'heads': head, 'tails': tail}
+    }
+}
 
-app.get('/app/flip/', (req, res) => {
-    res.statusCode = 200
-    const json = { "flip" : coinFlip() }
-    res.status(res.statusCode)
-    res.setHeader('Content-Type', 'application/json')
-    res.json(json)
-    
+
+function flipACoin(call) {
+    let flip = coinFlip()
+    const result = ""
+    if(call == flip) return {call: call, flip: flip, result: 'win'}
+    else return {call: call, flip: flip, result: 'lose'}
+}
+
+
+
+
+app.get('/app/', (req, res)=>{
+    res.statusCode = 200;
+    res.statusMessage = "OK"
+    res.writeHead(res.statusCode, {'Content-Type' : 'text/plain'});
+    res.end(res.statusCode+ ' ' +res.statusMessage)
+});
+
+app.get('/app/flip/', (req, res)=>{
+    res.status(200).json({'flip' : coinFlip()})
 });
 
 app.get('/app/flips/:number', (req, res) => {
-    res.statusCode = 200
-    const raw = coinFlips(req.params.number || 1)
-    const summary = countFlips(raw)
-    const json = {
-        "raw": raw,
-        "summary": summary
-    }
-    res.setHeader('Content-Type', 'application/json')
-    res.json(json)
-})
+    const flips = coinFlips(req.params.number)
+    res.status(200).json({'raw' : flips, 'summary' : countFlips(flips)})
+});
 
-app.get('/app/flip/call/:call', (req, res) => {
-    res.statusCode = 200
-    const json = flipACoin(req.params.call)
-    res.setHeader('Content-Type', 'application/json')
-    res.json(json)
-})
+app.get('/app/flip/call/heads', (req, res) =>{
+    res.status(200).send(flipACoin('heads'))
+});
+
+app.get('/app/flip/call/tails', (req, res) =>{
+    res.status(200).send(flipACoin('tails'))
+});
+
 app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+    res.status(404).send("404 NOT fOUND")
+    res.type("text/plain")
 });
-
-const server = app.listen(port, () => {
-        console.log('App listening on port %PORT%'.replace('%PORT%',port))
-});
-//functions
-function coinFlip() {
-    if(Math.random() > .5){
-      return "tails"
-    } else {
-      return "heads"
-    }
-  }
-
-  function coinFlips(flips) {
-    let returnable = [];
-    for(let i = 0; i < flips; i++){
-      returnable.push(coinFlip());
-    } 
-    return returnable;
-  }
-
-  function countFlips(array) {
-    let heads = 0;
-    let tails = 0;
-    for(let i = 0; i < array.length; i++){
-      if (array[i] == "heads"){
-        heads += 1;
-      } else {
-        tails += 1
-      }
-    }
-    return "{ heads: " + heads + ", tails: " + tails + " }"
-  }
-
-  function flipACoin(call) {
-    let er_mes = "Error: no input." + "\n"
-    let fix_mes = "Usage: node guess-flip --call=[heads|tails]"
-    if(call != "heads" && call != "tails"){
-      return er_mes + fix_mes
-    }
-    let theFlip = coinFlip();
-    let resultW = "win"
-    let resultsL = "lose"
-    if(theFlip == call){
-      return "{ call: " + call + ", flip: " + theFlip + ", result: " + resultW + " }"
-    } else {
-      return "{ call: " + call + ", flip: " + theFlip + ", result: " + resultsL + " }"
-    }
-  }
