9bbf89c527b40d39deb83f0ea5a3ac488a158470
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-05-02 01:46:59 -0400

    404

--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const server = app.listen(HTTP_PORT, () =>{
     console.log('App Listening on port %PORT%'.replace('%PORT%', HTTP_PORT))
 });
 
+//default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+});
