18bfb9588fcdffa4903095c63173f642d558b4b6
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-05-02 01:55:34 -0400

    functions

--- server.js
+++ server.js
@@ -18,7 +18,53 @@ app.use(function(req, res){
     res.status(404).send('404 NOT FOUND')
 });
 
+//functions
+function coinFlip() {
+    if(Math.random() > .5){
+      return "tails"
+    } else {
+      return "heads"
+    }
+  }
 
+  function coinFlips(flips) {
+    let returnable = [];
+    for(let i = 0; i < flips; i++){
+      returnable.push(coinFlip());
+    } 
+    return returnable;
+  }
+
+  export function countFlips(array) {
+    let heads = 0;
+    let tails = 0;
+    for(let i = 0; i < array.length; i++){
+      if (array[i] == "heads"){
+        heads += 1;
+      } else {
+        tails += 1
+      }
+    }
+    return "{ heads: " + heads + ", tails: " + tails + " }"
+  }
+
+  function flipACoin(call) {
+    let er_mes = "Error: no input." + "\n"
+    let fix_mes = "Usage: node guess-flip --call=[heads|tails]"
+    if(call != "heads" && call != "tails"){
+      return er_mes + fix_mes
+    }
+    let theFlip = coinFlip();
+    let resultW = "win"
+    let resultsL = "lose"
+    if(theFlip == call){
+      return "{ call: " + call + ", flip: " + theFlip + ", result: " + resultW + " }"
+    } else {
+      return "{ call: " + call + ", flip: " + theFlip + ", result: " + resultsL + " }"
+    }
+  }
+
+  
 
 app.get('/app/', (req, res) => {
     //status 200
